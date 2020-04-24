2dacbc3bee7598dda47b61bc2eb4c08bddc43aba
unknown <kyle.xiaoping@gmail.com>
2020-04-24 11:51:23 -0400

    V0.1

---
+++ example.js
@@ -0,0 +1,5 @@
+let text = document.getElementById('HereIsWhereTextIsAdded').innerText;
+
+function addText(){
+	document.getElementById('HereIsWhereTextIsAdded').innerText = "This is V0.1. The maker of this website is lazy and decided not to add anything."
+}
